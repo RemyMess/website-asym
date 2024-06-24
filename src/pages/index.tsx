9dbe55cc6e7235e484a8f329f1787eb46ce3add1
remymess <remy.messadene@gmail.com>
2024-06-24 16:36:02 +0100

    Updating metatag website

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <Seo
         title="Mechadoc"
-        description="AI Copilot for mech teams."
+        description="Automating compliance for hardware."
       />
       <Hero />
     </>
